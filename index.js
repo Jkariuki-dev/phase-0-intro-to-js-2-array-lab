c844c564ba354c933a9c7cfc6c779b2a720be83d
James <james.kariuki2@student.moringaschool.com>
2024-01-18 08:47:29 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+function destructivelyPrependCat(Ralph) {
+    cats.unshift("Oscar");
+  }
+  
+  destructivelyPrependCat('Whiskers');
+
+    let cats = ["Milo", "Otis", "Garfield"];
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+  }  
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+  }
+  function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+  }
+  function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+  }
+  function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+  }
+  function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+  }
